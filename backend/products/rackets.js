2282911c258e621ae39ce70aaa8caeda861b90d3
Octavio Salas <salasoctavio129@gmail.com>
2023-05-23 09:17:47 -0300

    prod

---
+++ backend/products/rackets.js
@@ -0,0 +1,84 @@
+const rackets =  [
+    { 
+        "id": 1,
+        "name": "Head Reward",
+        "price": 122000,
+        "category": "rackets",
+        "brand": "head",
+        "img": "https://d3ugyf2ht6aenh.cloudfront.net/stores/002/050/152/products/235621_ti_reward_mm_usa_trade_nube1-fca0aedd6bbe4b564d16467418819607-640-0.webp"
+    }, 
+    { 
+        "id": 2,
+        "name": "Head Radical",
+        "price": 120190,
+        "category": "rackets",
+        "brand": "head",
+        "img": "https://d3ugyf2ht6aenh.cloudfront.net/stores/002/050/152/products/234111_radical_mp_2021_catalogo_nube1-612eeb291b7bd39c1316467568870136-480-0.webp"
+    }, 
+    { 
+        "id": 3,
+        "name": "Babolat Aero Rafa",
+        "price": 113200,
+        "category": "rackets",
+        "brand": "babolat",
+        "img": "https://http2.mlstatic.com/D_NQ_NP_687445-MLA49878551923_052022-O.webp"
+    }, 
+    { 
+        "id": 4,
+        "name": "Head Graphene 360+ Instinct Mp Grafito",
+        "price": 98700,
+        "category": "rackets",
+        "brand": "head",
+        "img": "https://http2.mlstatic.com/D_NQ_NP_893041-MLU69331405768_052023-O.webp"
+    }, 
+    { 
+        "id": 5,
+        "name": "Babolat Pure Strike 16x19 ",
+        "price": 131700,
+        "category": "rackets",
+        "brand": "babolat",
+        "img": "https://http2.mlstatic.com/D_NQ_NP_689314-MLA40350616876_012020-O.webp"
+    }, 
+    { 
+        "id": 6,
+        "name": "Wilson Pro Staff Precision 103 G3",
+        "price": 70200,
+        "category": "rackets",
+        "brand": "wilson",
+        "img": "https://sporting.vtexassets.com/arquivos/ids/510154-1200-1200?v=637859016992770000&width=1200&height=1200&aspect=true"
+    }, 
+    { 
+        "id": 7,
+        "name": "Yonex Vcore 100 2023",
+        "price": 118100,
+        "category": "rackets",
+        "brand": "babolat",
+        "img": "https://http2.mlstatic.com/D_NQ_NP_875962-MLA69500756770_052023-O.webp"
+    }, 
+    { 
+        "id": 8,
+        "name": "Babolat Pure Aero 2023",
+        "price": 122600,
+        "category": "rackets",
+        "brand": "babolat",
+        "img": "https://www.onlytenis.com/24488-large_default/raqueta-babolat-pure-aero-300-gr-2023.jpg"
+    }, 
+    { 
+        "id": 9,
+        "name": "Tecnifibre Tfight 305 Isoflex",
+        "price": 108900,
+        "category": "rackets",
+        "brand": "tecnifebre",
+        "img": "https://http2.mlstatic.com/D_NQ_NP_673282-MLA69543639321_052023-O.webp"
+    }, 
+    { 
+        "id": 10,
+        "name": "TENIS HEAD BOOM TEAM L",
+        "price": 116000,
+        "category": "rackets",
+        "brand": "head",
+        "img": "https://d3ugyf2ht6aenh.cloudfront.net/stores/002/050/152/products/233532_boom-team-l_11-c5e6e34a348c9502b016747517469855-480-0.webp"
+    }
+]
+
+export default rackets;
---
+++ backend/products/tshirts.js
@@ -0,0 +1,84 @@
+const tshirts =  [
+    { 
+        "id": 1,
+        "name": "Babolat new Sportman",
+        "price": 17000,
+        "category": "tshirts",
+        "brand": "babolat",
+        "img": "https://s3.sa-east-1.amazonaws.com/www.vaypol.com.ar/variants/bl4hs6z0u5c0nuudy54xukj3y22h/c77c2a06864ac9aca38dc5bd9371de015471edcdbf322dfb14411689bf968ae5"
+    }, 
+    { 
+        "id": 2,
+        "name": " Own the Run ",
+        "price": 21190,
+        "category": "tshirts",
+        "brand": "adidas",
+        "img": "https://sporting.vtexassets.com/arquivos/ids/619670-1200-1200?v=637980580744600000&width=1200&height=1200&aspect=true"
+    }, 
+    { 
+        "id": 3,
+        "name": "Aero React Rafa Nadal",
+        "price": 23200,
+        "category": "tshirts",
+        "brand": "nike",
+        "img": "https://http2.mlstatic.com/D_NQ_NP_881157-MLA43222396729_082020-O.webp"
+    }, 
+    { 
+        "id": 4,
+        "name": "Push Up RF",
+        "price": 16700,
+        "category": "tshirts",
+        "brand": "nike",
+        "img": "https://http2.mlstatic.com/D_NQ_NP_709833-MLA52333547573_112022-W.jpg"
+    }, 
+    { 
+        "id": 5,
+        "name": "Sublimate Stricker",
+        "price": 13700,
+        "category": "tshirts",
+        "brand": "head",
+        "img": "https://www.puntosport.com.ar/11202-thickbox_default/remera-head-stricker-sublimada-tenis-padel-azul-blanco-verde-gris-rp00012.jpg"
+    }, 
+    { 
+        "id": 6,
+        "name": "Babolat Sensity",
+        "price": 10200,
+        "category": "tshirts",
+        "brand": "babolat",
+        "img": "https://media.solodeportes.com.ar/media/catalog/product/cache/7c4f9b393f0b8cb75f2b74fe5e9e52aa/r/e/remera-de-tenis-babolat-air-blanca-55041923-720021116530101-1.jpg"
+    }, 
+    { 
+        "id": 7,
+        "name": "Nike RF",
+        "price": 19100,
+        "category": "tshirts",
+        "brand": "nike",
+        "img": "https://pbs.twimg.com/media/ESxsRJ0XsAMr8Me.jpg"
+    }, 
+    { 
+        "id": 8,
+        "name": "Uniqlo Rf",
+        "price": 22600,
+        "category": "tshirts",
+        "brand": "uniqlo",
+        "img": "https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fimage%2F2019%2F06%2Funiqlo-roger-federer-wimbledon-tennis-kit-merch-02.jpg"
+    }, 
+    { 
+        "id": 9,
+        "name": "Nike Under Team",
+        "price": 19900,
+        "category": "tshirts",
+        "brand": "nike",
+        "img": "https://www.sportit.com/ir/34639/f/f/0/600/0/1427378726730/polo-premier-roger-federer.jpg"
+    }, 
+    { 
+        "id": 10,
+        "name": "Adidas Barricade",
+        "price": 116000,
+        "category": "rackets",
+        "brand": "adidas",
+        "img": "https://http2.mlstatic.com/D_994126-MLA31024204659_062019-O.jpg"
+    }
+]
+
+export default rackets;
